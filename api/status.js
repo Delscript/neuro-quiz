6f1768f0aefb8f4797667e45342a77230c99ef13
Delscript <delreis61@gmail.com>
2026-02-11 11:46:16 -0300

    Update status.js

--- api/status.js
+++ api/status.js
@@ -118,4 +118,4 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ erro: 'Erro interno' });
     }
-                                  }
+}
